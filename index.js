434451dbd119e34301b7117a1fa62767b7e6955b
brooktewabe <brooktewabe00@gmail.com>
2024-10-25 10:23:55 +0300

    'feat:handle-non-existing-endpoints'

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.use(bodyParser.json());
 
 app.use('/person', personRoutes);
 
+// 404 Handler for non-existing routes
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Not Found",  // Custom message for 404 error
+        requestedUrl: req.originalUrl
+    });
+});
 
 if (require.main === module) {
     app.listen(3000, () => console.log('Server is running on port 3000'));
